1af27950fde1655b7df57e4b4f5e0dd7fefd9f07
Filip Gerhát <filip.gerhat.1@gmail.com>
2021-12-19 15:52:52 +0100

    hts221 driver now uses calibration values when reading temp/humidity

--- Core/Inc/temp_humidity_sensor/hts221.h
+++ Core/Inc/temp_humidity_sensor/hts221.h
@@ -21,6 +21,21 @@
 #define HTS221_REG_TEMP_OUT_L 0x2A
 #define HTS221_REG_TEMP_OUT_H 0x2B
 
+// calibration registers
+#define HTS221_REG_H0_rH_x2 0x30
+#define HTS221_REG_H1_rH_x2 0x31
+#define HTS221_REG_T0_degC_x8 0x32
+#define HTS221_REG_T1_degC_x8 0x33
+#define HTS221_REG_T1_T0_MSB 0x35
+#define HTS221_REG_H0_T0_OUT_L 0x36
+#define HTS221_REG_H0_T0_OUT_H 0x37
+#define HTS221_REG_H1_T0_OUT_L 0x3A
+#define HTS221_REG_H1_T0_OUT_H 0x3B
+#define HTS221_REG_T0_OUT_L 0x3C
+#define HTS221_REG_T0_OUT_H 0x3D
+#define HTS221_REG_T1_OUT_L 0x3E
+#define HTS221_REG_T1_OUT_H 0x3F
+
 
 void hts221_write_byte(uint8_t reg_address, uint8_t data);
 uint8_t hts221_read_byte(uint8_t reg_address);
--- Core/Src/temp_humidity_sensor/hts221.c
+++ Core/Src/temp_humidity_sensor/hts221.c
@@ -8,6 +8,12 @@
 #include "temp_humidity_sensor/hts221.h"
 
 static uint8_t address = HTS221_I2C_ADDRESS;
+static float tx0, tx1, ty0, ty1; // temperature calibration data
+static float hx0, hx1, hy0, hy1; // humidity calibration data
+
+static float linear_interp(float x, float x0, float x1, float y0, float y1);
+
+
 
 void hts221_write_byte(uint8_t reg_address, uint8_t data)
 {
@@ -29,8 +35,41 @@ uint8_t hts221_init()
 		return 0;
 	}
 
-	// set up sensor registers
+	// read calibration data
+	uint8_t h0_rh_x2 = hts221_read_byte(HTS221_REG_H0_rH_x2);
+	uint8_t h1_rh_x2 = hts221_read_byte(HTS221_REG_H1_rH_x2);
+	uint8_t t0_degC_x8_l = hts221_read_byte(HTS221_REG_T0_degC_x8);
+	uint8_t t1_degC_x8_l = hts221_read_byte(HTS221_REG_T1_degC_x8);
+	uint8_t t1_t0_msb = hts221_read_byte(HTS221_REG_T1_T0_MSB);
+	uint8_t h0_t0_out_l = hts221_read_byte(HTS221_REG_H0_T0_OUT_L);
+	uint8_t h0_t0_out_h = hts221_read_byte(HTS221_REG_H0_T0_OUT_H);
+	uint8_t h1_t0_out_l = hts221_read_byte(HTS221_REG_H1_T0_OUT_L);
+	uint8_t h1_t0_out_h = hts221_read_byte(HTS221_REG_H1_T0_OUT_H);
+	uint8_t t0_out_l = hts221_read_byte(HTS221_REG_T0_OUT_L);
+	uint8_t t0_out_h = hts221_read_byte(HTS221_REG_T0_OUT_H);
+	uint8_t t1_out_l = hts221_read_byte(HTS221_REG_T1_OUT_L);
+	uint8_t t1_out_h = hts221_read_byte(HTS221_REG_T1_OUT_H);
 
+	int16_t t0_out = t0_out_l | (t0_out_h << 8);
+	int16_t t1_out = t1_out_l | (t1_out_h << 8);
+	int16_t t0_degC_x8 = t0_degC_x8_l | ((t1_t0_msb & 0b11) << 8);
+	int16_t t1_degC_x8 = t1_degC_x8_l | ((t1_t0_msb & 0b1100) << (8 - 2));
+
+	tx0 = (float)t0_out;
+	tx1 = (float)t1_out;
+	ty0 = (float)t0_degC_x8/8.0;
+	ty1 = (float)t1_degC_x8/8.0;
+
+	int16_t h0_t0_out = h0_t0_out_l | (h0_t0_out_h << 8);
+	int16_t h1_t0_out = h1_t0_out_l | (h1_t0_out_h << 8);
+
+	hx0 = (float)h0_t0_out;
+	hx1 = (float)h1_t0_out;
+	hy0 = (float)h0_rh_x2/2.0;
+	hy1 = (float)h1_rh_x2/2.0;
+
+
+	// set up sensor registers
 	uint8_t ctrl_reg1 = 0b10000011;
 	hts221_write_byte(HTS221_REG_CTRL_REG1, ctrl_reg1);
 
@@ -45,7 +84,7 @@ float hts221_get_temperature()
 
 	int16_t temp = temp_l | (temp_h << 8);
 
-	float temperature = (float)temp/8.0;
+	float temperature = linear_interp((float)temp, tx0, tx1, ty0, ty1);
 
 	if(temperature > 120)
 	{
@@ -67,7 +106,7 @@ float hts221_get_humidity()
 
 	int16_t hum = hum_l | (hum_h << 8);
 
-	float humidity = (float)hum/2.0;
+	float humidity = linear_interp((float)hum, hx0, hx1, hy0, hy1);
 
 	if(humidity > 100)
 	{
@@ -79,4 +118,10 @@ float hts221_get_humidity()
 	}
 
 	return humidity;
+}
+
+static float linear_interp(float x, float x0, float x1, float y0, float y1)
+{
+	// https://www.vedantu.com/formula/linear-interpolation-formula
+	return y0 + ((x-x0)*(y1-y0))/(x1-x0);
 }
